aea0201b163f5365397ea091a64068c7300593c0
YeonJuJu <98uplay@naver.com>
2021-04-15 16:48:42 +0900

    DB connect
    오라클 데이터베이스 연결

--- JangYuYuSeo/src/com/jangyuyuseo/spring/config/ServletAppContext.java
+++ JangYuYuSeo/src/com/jangyuyuseo/spring/config/ServletAppContext.java
@@ -1,52 +1,78 @@
 package com.jangyuyuseo.spring.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 // Spring MVC 프로젝트에 관련된 설정을 하는 클래스
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.jangyuyuseo.spring.controller", "com.jangyuyuseo.spring.bean"})
+@ComponentScan(basePackages = { "com.jangyuyuseo.spring.controller", "com.jangyuyuseo.spring.dao",
+		"com.jangyuyuseo.spring.service" })
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
 	
-  /*
-    Controller 에서 return하는 문자열(경로)에 접두사, 접미사 설정하기 
-  */
-  @Override
-  public void configureViewResolvers(ViewResolverRegistry registry) {
-    WebMvcConfigurer.super.configureViewResolvers(registry);
-    registry.jsp("/WEB-INF/view/",".jsp");
-  }
-  
-  /*
-    프로젝트에서 사용하는 정적 파일 경로 설정 (이미지, 사운드, 동영상, js, css 등) 
-  */
-  @Override
-  public void addResourceHandlers
-    (ResourceHandlerRegistry registry) {
-    WebMvcConfigurer.super.addResourceHandlers(registry);
-    registry.addResourceHandler("/**").addResourceLocations("/resources/");
-  }
-  
-  
+	@Value("${db.classname}")
+	private String db_classname;
+	
+	@Value("${db.url}")
+	private String db_url;
+	
+	@Value("${db.username}")
+	private String db_username;
+	
+	@Value("${db.password}")
+	private String db_password;
+	
+	/*
+	 * Controller 에서 return하는 문자열(경로)에 접두사, 접미사 설정하기
+	 */
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+
+	/*
+	 * 프로젝트에서 사용하는 정적 파일 경로 설정 (이미지, 사운드, 동영상, js, css 등)
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+	
+	/*
+	 * DB 접속 관리 Bean
+	 */
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+		return source;
+	}
+	/*
+	 * Query와 접속정보 관리 객체
+	 */
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception{
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+		return factory;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
